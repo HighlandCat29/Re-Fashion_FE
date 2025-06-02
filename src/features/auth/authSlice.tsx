cb42cfa1f3a1c3bcdcd5ed300d88f32d715ae2c5
HighlandCat29 <minecaftlichking@gmail.com>
2025-06-02 13:06:47 +0700

    merge

--- src/features/auth/authSlice.tsx
+++ src/features/auth/authSlice.tsx
@@ -4,22 +4,6 @@ type AuthState = {
   loginStatus: boolean;
 };
 
-<<<<<<< HEAD
-
-const getUserFromStorage = (): boolean => {
-  try {
-    const storedUser = localStorage.getItem("user");
-    if (!storedUser) return false;
-
-    const parsedUser = JSON.parse(storedUser);
-
-    // Check that 'id' exists and is not undefined/null/empty
-    return Boolean(parsedUser && parsedUser.id);
-  } catch (error) {
-    console.error("Invalid JSON in localStorage 'user':", error);
-    return false;
-  }
-=======
 const initialState: AuthState = {
   loginStatus: (() => {
     try {
@@ -31,12 +15,8 @@ const initialState: AuthState = {
       return false;
     }
   })(),
->>>>>>> c7c3f1f18b9c9f81a610d74556076dd9515259c8
 };
 
-const initialState: AuthState = {
-  loginStatus: getUserFromStorage(),
-};
 export const authSlice = createSlice({
   name: "auth",
   // `createSlice` will infer the state type from the `initialState` argument
