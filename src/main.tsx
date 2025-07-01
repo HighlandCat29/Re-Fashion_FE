f0ab164d39145f43df0e7bdc558d1ef2c1571b31
SamroyIsMe <dhonghai191204@gmail.com>
2025-07-02 03:14:40 +0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import "./index.css";
 import { store } from "./store.ts";
 import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-VFT2X5B6GN");
+ReactGA.send("pageview");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
